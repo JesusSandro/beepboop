6a6803c495126774d35f65e72abdc71cc30ea590
JesusSandro <sandro_t_d@hotmail.com>
2017-04-07 16:49:07 +0100

    Add files via upload

--- beepboop.js
+++ beepboop.js
@@ -8,26 +8,24 @@ const streamOptions = { seek: 0, volume: 1 };
 
 var fs = require('fs');
 
-var opusscript = require("opusscript");
-
-
-
-// 48kHz sampling rate, 20ms frame duration, stereo audio (2 channels) 
-var samplingRate = 48000;
-var frameDuration = 20;
+var opus = require('node-opus');
+ 
+// Create the encoder. 
+// Specify 48kHz sampling rate and 10ms frame size. 
+// NOTE: The decoder must use the same values when decoding the packets. 
+var rate = 48000;
+var encoder = new opus.OpusEncoder( rate );
+ 
+// Encode and decode. 
+var frame_size = rate/100;
+var encoded = encoder.encode( buffer, frame_size );
+var decoded = encoder.decode( encoded, frame_size );
+ 
+// or create streams 
 var channels = 2;
-
-// Optimize encoding for audio. Available applications are VOIP, AUDIO, and RESTRICTED_LOWDELAY 
-var encoder = new opusscript(samplingRate, channels, opusscript.Application.AUDIO);
-
-var frameSize = samplingRate * frameDuration / 1000;
-
-// Get PCM data from somewhere and encode it into opus 
-var pcmData = new Buffer('heh.wav');
-var encodedPacket = encoder.encode(pcmData, frameSize);
-
-// Decode the opus packet back into PCM 
-var decodedPacket = encoder.decode(encodedPacket);
+var opusEncodeStream = new opus.Encoder(rate, channels, frame_size);
+var opusDecodeStream = new opus.Decoder(rate, channels, frame_size);
+// see examples folder for a more complete example 
 
 
 var token = "MjMwODE4Mjc5Nzg1MTAzMzYx.C71EJA.rZI_RHYY8FbLd0SM3uMLz6QeV5k";
